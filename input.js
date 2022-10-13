d646046ba3ff089f4e10197362cb74505229916f
Callis Ezenwaka <callisezenwaka@gmail.com>
2022-10-13 20:28:16 +0100

    Add notes

---
+++ input.js
@@ -0,0 +1,47 @@
+export const json = [
+  {
+    date: 20210307,
+    positives: 28756184,
+    fatalities: 515148,
+  },
+  {
+    date: 20210306,
+    positives: 28714654,
+    fatalities: 514309,
+  },
+  {
+    date: 20210305,
+    positives: 28654639,
+    fatalities: 512629,
+  },
+  {
+    date: 20210304,
+    positives: 28585852,
+    fatalities: 510408,
+  },
+  {
+    date: 20210303,
+    positives: 28520365,
+    fatalities: 508665,
+  },
+  {
+    date: 20210302,
+    positives: 28453529,
+    fatalities: 506216,
+  },
+  {
+    date: 20210301,
+    positives: 28399281,
+    fatalities: 504488,
+  },
+];
+
+export const csv = `
+  date,positives,fatalities
+  20210307,28756184,515148
+  20210306,28714654,514309
+  20210305,28654639,512629
+  20210304,28585852,510408
+  20210303,28520365,508665
+  20210302,28453529,506216
+  20210301,28399281,504488`;
